2414d93ed87e83df7219b2ac9be5e3d2df3fd489
unknown <Administrator@WLT1-DLIN5759.SourceITGH.com>
2021-10-29 06:26:44 +0000

    server bootstrapped succesfully

---
+++ index.js
@@ -0,0 +1,59 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+//schema
+//resolvers
+
+const books = [
+    {
+        title: "James Walker",
+        author: "James Dzah",
+        ISBN: "0-7475-3269-9"
+    },
+    {
+        title: "Micheal Scotch",
+        author: "The whitemen",
+        ISBN: "0-7475-3269-9"
+    },
+];
+// type NameOftheType{
+// fieldName: type
+// }
+
+const schemas = gql`
+type Book {
+    title: String!
+    author: String!
+    ISBN: String
+}
+type Query {
+    books: [Book]
+    book(title: String!): Book
+}
+type Mutation {
+createBook(title: String!, author: String!, ISBN: String): Book
+}
+#type Subscription {}
+    `;
+
+const booksResolvers= {
+    Query: {
+        books: () => books,
+        book: (parent, args) => books.find(book => book.title == args.title)
+
+    },
+    Mutation: {
+        createBook: (parent, args) => {
+            const book = { title, author, ISBN } = args;
+            books.push(book);
+            return book;
+        }
+    },
+}
+
+
+const server = new ApolloServer({ typeDefs: schemas, resolvers: booksResolvers });
+
+server.listen(8000).then(({ url, port }) => {
+    console.log(`server ready at ${url} and ready to be used`);
+}).catch(err => console.log(err));
+
